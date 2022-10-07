20a8f023869db94ad5cec46b824129a70a247340
omthakur1224 <omthakur1224@gmail.com>
2022-10-07 15:25:38 +0530

    lec

--- Coding/sprint-1/day-1/lec/1.js
+++ Coding/sprint-1/day-1/lec/1.js
@@ -1,5 +1,6 @@
 const path=require('path');
 const { sum, mul, div, sub, mod } = require('./src/math');
+
 console.log(sum(12,14));
 console.log(sub(24,12));
 console.log(mul(2,4));
@@ -7,4 +8,5 @@ console.log(div(234,2));
 console.log(mod(254,10))
 
 console.log(process.cwd());
+
 console.log(path.join(process.cwd(),'..','day-2'))
