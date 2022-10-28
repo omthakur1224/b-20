50699173f0fe44106f7d6d85c4fb23d3ef423f92
omthakur1224 <omthakur1224@gmail.com>
2022-10-28 08:32:59 +0530

    minor changes

--- Coding/PP Library/crud_oprations/routes/user.routes.js
+++ Coding/PP Library/crud_oprations/routes/user.routes.js
@@ -4,50 +4,75 @@ const userRouter=express.Router();
 app.use(express.json());
 
 const fs=require('fs');
+
 const { dirname } = require("path");
+
 const path=require('path');
+
 var data=fs.readFileSync('./db.json','utf-8');
 
 let dbusers=JSON.parse(data);
 
+//get method
 userRouter.get('/',(req,res)=>{
+
     console.log("we are here to get data")
     res.send(dbusers)
 })
+
+//post method
 userRouter.post('/',(req,res)=>{
     console.log(req.body)
-    // let update=users;
+  
     let data=fs.readFileSync('db.json','utf-8');
+
      let user=JSON.parse(data);
+
      user=user.users;
+
     user.push({...req.body,"id":Date.now()});
+
     {dbusers.users=user}
+
     fs.writeFile('db.json',JSON.stringify(dbusers),()=>{});
     res.status(201).send('successfully created');
 })
+
+//delete method 
 userRouter.delete('/:id',(req,res)=>{
     let {id}=req.params;
-    // console.log(id)
+
     let data=dbusers.users;
+
     let newData=data.filter((user)=>user.id!=id);
 
     let dbdata=fs.readFileSync('db.json','utf-8');
+
     let user=JSON.parse(dbdata);
+
     user=user.users;
-//    user.push({...req.body,"id":Date.now()});
+
    {dbusers.users=newData}
+
    fs.writeFile('db.json',JSON.stringify(dbusers),()=>{});
    res.status(201).send('Deleted Successfully');
     
 })
+
+//update method
 userRouter.patch("/:id",(req,res)=>{
     let id=req.params.id;
-    console.log(id)
+
     let {username}=req.body;
+
     let dbdata=fs.readFileSync('db.json','utf-8');
+
     let user=JSON.parse(dbdata);
+
     user=user.users;
+
     dbusers.users.map((user)=>{if(user.id==id){user.username=username}});
+
     fs.writeFile('db.json',JSON.stringify(dbusers),()=>{});
     res.status(201).send('updated Successfully');
 })
--- Coding/PP Library/crud_oprations/src/index.js
+++ Coding/PP Library/crud_oprations/src/index.js
@@ -1,8 +1,15 @@
 const express=require('express');
+
 const app=express();
+
 app.use(express.json());
+
 app.use('/users',require('../routes/user.routes'))
+
 const PORT=8000;
+
 app.listen(PORT,(req,res)=>{
-    console.log(`server is running in PORT ${PORT}`)
+
+    console.log(`server is running in PORT ${PORT}`);
+
 })
